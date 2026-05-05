7735cfb438f5d51f3b85be8f75277346f4486f95
Einar W. Arbo <earb@equinor.com>
2026-05-05 10:07:18 +0000

    chore: use consistent numpy-style docstrings
    Mirrors equinor/rock-physics#206 conventions for the public type stubs:
    - Remove per-arg type annotations from docstrings (types live in the signature).
    - Each parameter as 'name\n    description'.
    - Use bullet lists for the scenario enum and output columns so they render with numpydoc.
    - Returns section uses 'type\n    description' style.

--- src/tmatrix/__init__.pyi
+++ src/tmatrix/__init__.pyi
@@ -19,40 +19,43 @@ def tmatrix_porosity(
 
     Parameters
     ----------
-    out_np: npt.NDArray[np.float64]
-        Stores the output result.
-    dim: int
+    out_np
+        Stores the output result. Shape should be (dim, 4).
+    dim
         Dimension of the output array.
-    mineral_property_np: npt.NDArray[np.float64]
-        Contains mineral bulk modulus [Pa], shear modulus [Pa] and density [kg/m³]. Shape should be (N, 3).
-    fluid_property_np: npt.NDArray[np.float64]
-        Contains fluid bulk modulus [Pa] and density [kg/m³], viscosity [cP] and permeability [mD]. Shape should be (N, 4).
-    phi_vector_np: npt.NDArray[np.float64]
-        Porosity values array. Shape should be (N,).
-    in_scenario: Literal[1, 2, 3, 4]
-        1: Dual porosity, mostly rounded pores
-        2: Dual porosity, little rounded pores
-        3: Mixed pores
-        4: Flat pores and cracks
-    frequency: float
-        Signal frequency [Hz]
-    angle_of_sym_plane: float
-        Angle of symmetry plane (0 = HTI, 90 = VTI medium) [deg]
-    per_inc_con: float
+    mineral_property_np
+        Contains mineral bulk modulus [Pa], shear modulus [Pa] and density
+        [kg/m³]. Shape should be (dim, 3).
+    fluid_property_np
+        Contains fluid bulk modulus [Pa] and density [kg/m³], viscosity [cP]
+        and permeability [mD]. Shape should be (dim, 4).
+    phi_vector_np
+        Porosity values array. Shape should be (dim,).
+    in_scenario
+        Pore-shape scenario:
+
+        - 1: Dual porosity, mostly rounded pores
+        - 2: Dual porosity, little rounded pores
+        - 3: Mixed pores
+        - 4: Flat pores and cracks
+    frequency
+        Signal frequency [Hz].
+    angle_of_sym_plane
+        Angle of symmetry plane (0 = HTI, 90 = VTI medium) [deg].
+    per_inc_con
         Fraction of inclusions that are connected.
-    per_inc_any: float
-        Fraction of inclusions that are anisotropic
+    per_inc_any
+        Fraction of inclusions that are anisotropic.
 
     Returns
     -------
-    int
-        0 if success, otherwise failure.
-        Result will be stored in `out_np`. Output array has shape (out_N, 4).
-        Column values in order are:
-            Vp: Vertical P-wave velocity [m/s]
-            Vsv: Vertical polarity S-wave velocity [m/s]
-            Vsh: Horizontal polarity S-wave velocity [m/s]
-            Rhob [kg/m^3]
+    Always ``0``. The result is stored in ``out_np`` with shape
+    (dim, 4). Columns in order are:
+
+    - Vp: Vertical P-wave velocity [m/s]
+    - Vsh: Horizontal polarity S-wave velocity [m/s]
+    - Vsv: Vertical polarity S-wave velocity [m/s]
+    - Rhob: Effective density [kg/m³]
     """
 
 def tmatrix_porosity_noscenario(
@@ -71,46 +74,51 @@ def tmatrix_porosity_noscenario(
     inc_ani_np: npt.NDArray[np.float64],
     inc_con_N: int,
 ) -> None:
-    """Compute TMatrix Porosity, No scenario.
+    """Compute TMatrix Porosity, no scenario.
 
     Parameters
     ----------
-    out_np: npt.NDArray[np.float64]
-        Stores the output result.
-    out_N: int
+    out_np
+        Stores the output result. Shape should be (out_N, 4).
+    out_N
         Dimension of the output array.
-    mineral_property_np: npt.NDArray[np.float64]
-        Contains mineral bulk modulus [Pa], shear modulus [Pa] and density [kg/m³]. Shape should be (N, 3).
-    fluid_property_np: npt.NDArray[np.float64]
-        Contains fluid bulk modulus [Pa] and density [kg/m³], viscosity [cP] and permeability [mD]. Shape should be (N, 4).
-    phi_vector_np: npt.NDArray[np.float64]
-        Porosity values array. Shape should be (N,) where N is the number of porosity values.
-    alpha_np: npt.NDArray[np.float64]
-        Aspect ratio values array. Shape should be (N,) where N is the number of aspect ratio values.
-    v_np: npt.NDArray[np.float64],
+    mineral_property_np
+        Contains mineral bulk modulus [Pa], shear modulus [Pa] and density
+        [kg/m³]. Shape should be (out_N, 3).
+    fluid_property_np
+        Contains fluid bulk modulus [Pa] and density [kg/m³], viscosity [cP]
+        and permeability [mD]. Shape should be (out_N, 4).
+    phi_vector_np
+        Porosity values array. Shape should be (out_N,).
+    alpha_np
+        Aspect ratio values array. Shape should be (N,) where N is the number
+        of aspect ratio values.
+    v_np
         Fraction of porosity with given aspect ratio.
-    alpha_size_np: npt.NDArray[np.int32],
-        Number of aspect ratio values per sample,
-    alpha_N: int
-        Length of `alpha_np`
-    frequency: float
-        Signal frequency [Hz]
-    angle: float
-        Angle of symmetry plane (0 = HTI, 90 = VTI medium) [deg]
-    inc_con_np: npt.NDArray[np.float64]
-        Fraction of inclusions that are connected. Should be numpy array with a single element.
-    inc_ani_np: npt.NDArray[np.float64]
-        Fraction of inclusions that are anisotropic. Should be numpy array with a single element.
-    inc_con_N: int
-        Length of `inc_con_np` and `inc_ani_np`
+    alpha_size_np
+        Number of aspect ratio values per sample.
+    alpha_N
+        Length of ``alpha_np``.
+    frequency
+        Signal frequency [Hz].
+    angle
+        Angle of symmetry plane (0 = HTI, 90 = VTI medium) [deg].
+    inc_con_np
+        Fraction of inclusions that are connected. Should be a numpy array
+        with a single element.
+    inc_ani_np
+        Fraction of inclusions that are anisotropic. Should be a numpy array
+        with a single element.
+    inc_con_N
+        Length of ``inc_con_np`` and ``inc_ani_np``.
 
     Returns
     -------
-    None
-        Result will be stored in `out_np`. Output array has shape (out_N, 4).
-        Column values in order are:
-            Vp: Vertical P-wave velocity [m/s]
-            Vsv: Vertical polarity S-wave velocity [m/s]
-            Vsh: Horizontal polarity S-wave velocity [m/s]
-            Rhob [kg/m^3]
+    The result is stored in ``out_np`` with shape (out_N, 4). Columns in
+    order are:
+
+    - Vp: Vertical P-wave velocity [m/s]
+    - Vsh: Horizontal polarity S-wave velocity [m/s]
+    - Vsv: Vertical polarity S-wave velocity [m/s]
+    - Rhob: Effective density [kg/m³]
     """
